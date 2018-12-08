19853c23f2d333a872fbb7364a84dd0906e0c319
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2018-12-07 23:58:48 -0600

    Coordinates complete

--- islands_engine/lib/islands_engine/coordinate.ex
+++ islands_engine/lib/islands_engine/coordinate.ex
@@ -1,9 +1,18 @@
 defmodule IslandsEngine.Coordinate do
+    @doc """
+    Represents an individual point in a board
+    """
+
     alias __MODULE__
+    @board_range 1..10
+
     @enforce_keys [:row, :col]
     defstruct [:row, :col]
 
-    def new(row, col) do
+    def new(row, col) when row in @board_range and col in @board_range do
         {:ok, %Coordinate{row: row, col: col}}
     end 
+
+    def new(_row, _col), do: {:error, :invalid_paramaters}
+
 end
