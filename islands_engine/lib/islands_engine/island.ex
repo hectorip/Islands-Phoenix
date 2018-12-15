c795736b41bb8e95bf0728cd43e741f72b7b478c
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2018-12-14 23:59:29 -0600

    WIP Islands

--- islands_engine/lib/islands_engine/island.ex
+++ islands_engine/lib/islands_engine/island.ex
@@ -2,5 +2,16 @@ defmodule IslandEngine.Island do
 
     @enforce_keys [:coordinates, :hit_coordinates]
     defstruct [:coordinates, :hit_coordinates]
+    def new(type, initial_coordinate) do
+        
+    end
 
+    def types(): [:atoll, :square, :l_shape, :s_shape]
+
+    def get_coordinates(:atoll, initial_coordinate), do
+        [{0, 0}]
+        |> generate_coordinates_from_transformations()
+    end
+
+    defp generate_coordinates_from_transformations()
 end
