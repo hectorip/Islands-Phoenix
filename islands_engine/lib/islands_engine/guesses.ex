a82d5c84ac4d58986ea8a5911947a02c2a4ea72b
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2018-12-18 23:59:32 -0600

    wip guesses

--- islands_engine/lib/islands_engine/guesses.ex
+++ islands_engine/lib/islands_engine/guesses.ex
@@ -1,7 +1,7 @@
 defmodule IslandsEngine.Guesses do
-    alias __MODULE__
-    @enforce_keys [:hits, :misses]
-    defstruct [:hits, :misses]
+  alias __MODULE__
+  @enforce_keys [:hits, :misses]
+  defstruct [:hits, :misses]
 
-    def new(), do: %Guesses{hits: %MapSet{}, misses: %MapSet{}}
+  def new(), do: %Guesses{hits: %MapSet{}, misses: %MapSet{}}
 end
